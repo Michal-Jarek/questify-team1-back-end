974636168ddab07fe9a4ac2f6683ee7bb8cad293
Dominika Sosnowska <domi.sosnowska@o2.pl>
2023-02-17 17:46:53 +0100

    data model

---
+++ modules/userModel.js
@@ -0,0 +1,53 @@
+const { model, Schema } = require("mongoose");
+
+const User = model(
+  "user",
+  new Schema({
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+    cards: [
+      {
+        title: {
+          type: String,
+          required: [true, "Title is required"],
+        },
+        difficulty: {
+          type: String,
+          enum: ["easy", "normal", "hard"],
+          default: "normal",
+        },
+        date: {
+          type: String,
+          default: "Today",
+        },
+        time: {
+          type: String,
+        },
+        status: {
+          type: String,
+          enum: ["Incomplete", "Complete"],
+          default: "Incomplete",
+        },
+        category: {
+          type: String,
+          enum: ["stuff", "family", "health", "learning", "leisure", "work"],
+          default: "stuff",
+        },
+        type: { type: String },
+      },
+    ],
+  })
+);
+
+module.exports = User;
